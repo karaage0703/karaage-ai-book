7ca8449c377e95eeafcf6a0ff83202639729b743
karaage <coldchicken@gmail.com>
2020-10-29 23:30:46 +0900

    Update script for adding label file

--- ch06/download.sh
+++ ch06/download.sh
@@ -4,5 +4,9 @@ curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1M1qcogcN8N
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1M1qcogcN8NTCXoyEDRVoBfE7e5DxxN56" -o my_model_aug.h5
 
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1-2aOVS4kUVMcz7OHSos372oqT2izr8aP" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1-2aOVS4kUVMcz7OHSos372oqT2izr8aP" -o labels.txt
+
 echo Download finished.
 
